883f91c872f4cac560e9d4efd73d958896bdde60
Rustam <pokataevrus@gmail.com>
2018-02-15 13:11:36 +0700

    Edit code

--- hello.c
+++ hello.c
@@ -1,24 +1,33 @@
+//
+//  main.c
+//  lab 1
+//
+//  Created by Rustamas on 14/02/2018.
+//  Copyright © 2018 Rustamas. All rights reserved.
+//
+
 #include <stdio.h>
 int main()
 {
-	file *f = fopen("doska.txt");
-	char T[10][10];
-	int i = 0, j = 0;
-	while(!feof(f))
-{
-	fscanf(f, %c, &T[i++][j]);
-	if(i==9)
-	{
-	i = 0; j++;
-	}
+    FILE *f = fopen("doska.txt","rt");
+    char T[10][10];
+    int i = 0, j = 0;
+    while(!feof(f))
+    {
+        fscanf(f, "%c", &T[i++][j]);
+        if(i==9)
+        {
+            i = 0; j++;
+        }
+    }
+    for(i=0; i<9; i++)
+    {
+        for(j=0; j<9; j++)
+        {
+            printf("%c",T[i][j]);
+        }
+        printf("/n");
+    }
+    return 0;
 }
-for(i=0; i<9; i++)
-{
-for(j=0; j<9; j++)
-{
-printf("%c",T[i][j])
-}
-printf("/n");
-}
-return 0;
 
